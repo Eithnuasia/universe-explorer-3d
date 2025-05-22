5b3d544151c47b87126154cbd6bfbcafcf99aa69
eithnuasia <you@example.com>
2025-05-22 12:28:27 +0700

    Fix: set basePath and assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: "/universe-explorer-3d",
+  assetPrefix: "/universe-explorer-3d/",
   eslint: {
     ignoreDuringBuilds: true,
   },
